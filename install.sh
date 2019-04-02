fedbcee063fae33ac24b65fb004f8cdd28342d10
Simon Shi <simon.shi@ibm.com>
2019-04-01 22:19:22 -0400

    update

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@ set -x
 INSTALLER_DIR=/icp4d_installer
 TERRAFORM_DIR=/terraform
 
-if [ $# -ne 3 ];then
+if [ $# -lt 1 ];then
     echo "usage: $0 [-g aws] [-i azure] [-f installer_name]"
     exit 1
 fi
+
+generate_conf = 0
+install = 0
 
 while getopts g:i:f: arg
 do
@@ -17,7 +20,7 @@ do
     i) install=1
        cloud=$OPTARG;;
     f) installer=$OPTARG;;
-    ?) echo "usage: $0 [-g aws] [-i azure]"
+    ?) echo "usage: $0 [-g aws] [-i azure] [-f installer_name]"
        exit 1
         ;;
   esac
@@ -26,16 +29,20 @@ shift $(($OPTIND-1))
 
 # generate install.tfvars file
 if [[ $generate_conf -eq 1 ]];then
-    if [[ "$cloud" == "aws"]];then
+    if [[ "$cloud" == "aws" ]];then
         cp $TERRAFORM_DIR/terraform-icp-aws/install.conf $INSTALLER_DIR/install.tfvars
     elif [[ "$cloud" == "azure" ]];then
-        cp $TERRAFORM_DIR/terraform-icp-azure/icp-ee-as/install.conf $INSTALLER_DIR/install.tfvars
+        cp $TERRAFORM_DIR/terraform-icp-azure/templates/icp-ee-as/install.conf $INSTALLER_DIR/install.tfvars
     else
         echo "not yet supported"
         exit 1
     fi
+    exit 0
 fi
 
+if [[ $install -ne 1 ]];then
+    exit 1
+fi
 # validate install.tfvars
 
 # extrac icp4d installer -i aws/azure
